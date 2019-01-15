99cc3c536ffcdd77848a8ce5da459bdc26915af4
Aaron McFaull <Aaron@Aarons-MacBook-Pro.local>
2019-01-15 17:04:32 +0000

    Seven Questions Solved

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,27 @@
 def return_10
   return 10
 end
+
+def add(firstnumber, secondnumber)
+  return firstnumber + secondnumber
+  end
+
+  def subtract(firstnumber, secondnumber)
+    return firstnumber - secondnumber
+    end
+
+    def multiply(firstnumber, secondnumber)
+      return firstnumber * secondnumber
+      end
+
+      def divide(firstnumber, secondnumber)
+        return firstnumber / secondnumber
+        end
+
+        def length_of_string(string)
+          return string.length
+          end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
